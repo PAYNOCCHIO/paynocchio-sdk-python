efe737347fa7eb265615e4ee822b6a7537e64e31
errand <errandvega@gmail.com>
2024-10-18 12:44:14 +0300

    refactor: go back to prod

--- paynocchio/client.py
+++ paynocchio/client.py
@@ -3,5 +3,5 @@ from paynocchio.api_client import Api
 
 class Client(Api):
     def __init__(self, api_key: str, environment_uuid: str, user_uuid: str, test_mode: str) -> None:
-        self.base_uri = 'wallet.stage.paynocchio.com'
+        self.base_uri = 'wallet.paynocchio.com'
         super(Client, self).__init__(api_key, environment_uuid, user_uuid, test_mode)
